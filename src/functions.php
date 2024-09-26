3aea8f68b96939e6a79f1e932df75ec4470d4482
ipranjal <pranjaltech10@gmail.com>
2024-09-27 03:02:42 +0530

    feat: url helper function

--- src/functions.php
+++ src/functions.php
@@ -12,3 +12,13 @@ if(!function_exists('config')){
         return \Scrawler\App::engine()->config();
     }
 }
+
+if (! function_exists('url')) {
+    function url($path='')
+    {
+        if (\Scrawler\App::engine()->config()->has('https') && \Scrawler\App::engine()->config()->get('https')) {
+            return 'https://'.\Scrawler\App::engine()->request()->getHttpHost().\Scrawler\App::engine()->request()->getBasePath().$path;
+        }
+        return \Scrawler\App::engine()->request()->getSchemeAndHttpHost().\Scrawler\App::engine()->request()->getBasePath().$path;
+    }
+}
