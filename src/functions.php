ed40adc26cca4f42e9dcf5651d5e3f3ffe665892
ipranjal <pranjaltech10@gmail.com>
2024-09-27 06:11:57 +0530

    fix: functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,14 +1,17 @@
 <?php
 
 if(!function_exists('app')){
-    if(is_null(\Scrawler\App::engine())){
-        new \Scrawler\App();
+    function app(){
+        if(is_null(\Scrawler\App::engine())){
+            new \Scrawler\App();
+        }
+        return \Scrawler\App::engine();
     }
-    return \Scrawler\App::engine();
+   
 }
 
 if(!function_exists('config')){
-    function app(){
+    function config(){
         return \Scrawler\App::engine()->config();
     }
 }
